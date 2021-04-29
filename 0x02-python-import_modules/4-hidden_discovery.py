f37faef8f428d6039040fdfc83530d7bf86eb303
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-29 15:42:55 +0000

    ewe

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import imp
-    compiled = imp.load_compiled("hidden_4","./hidden_4.pyc")
-    compiled_names = compiled.__dir__()
+    import hidden_4
+    compiled_names = dir(hidden_4)
     for i in range(0, len(compiled_names)):
         if compiled_names[i][0:2] != "__":
             print(compiled_names[i])
