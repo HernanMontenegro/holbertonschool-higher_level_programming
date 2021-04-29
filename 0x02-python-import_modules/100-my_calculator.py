44fba6180d932fa8692b8aa33400f816c26cbaec
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-29 17:01:18 +0000

    asd

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,17 +6,14 @@ if __name__ == "__main__":
     av_len = len(av)
     first = 0
     second = 0
-
     if (av_len != 4):
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif (av[2] != "+" and av[2] != "-" and av[2] != "*" and av[2] != "/"):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
     first = int(av[1])
     second = int(av[3])
-
     if (av[2] == "+"):
         print("{:d} + {:d} = {:d}".format(first, second, first + second))
     if (av[2] == "-"):
