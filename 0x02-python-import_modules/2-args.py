e6c9f4b2f34556ea66223a51b997b62a608a29ea
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-04-29 15:35:15 +0000

    2 fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     av_len = len(sys.argv)
     if av_len == 1:
         print("0 arguments.")
-    else:
-        print("{:d} arguments:".format(av_len - 1))
-        for i in range(1, av_len):
-            print("{:d}: {}".format(i, sys.argv[i]))
+        exit()
+    elif av_len == 2:
+        print("1 argument:")
+        print("{:d}: {}".format(1, sys.argv[1]))
+        exit()
+    print("{:d} arguments:".format(av_len - 1))
+    for i in range(1, av_len):
+        print("{:d}: {}".format(i, sys.argv[i]))
