3985f061f00dcbcd6c0a7e24e6b2618425a3818a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-04 15:26:57 -0300

    6 solved

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print(matrix[i][j], end='')
+            if (j != len(matrix[i]) - 1):
+                print(" ", end='')
+        print("")
