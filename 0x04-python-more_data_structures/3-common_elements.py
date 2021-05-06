0d5f75b6113a5a972fd284b4b796cacf395b619a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-06 18:27:24 -0300

    asd

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-	list_1 = list(set_1)
-	list_2 = list(set_2)
-	new_list = []
+    list_1 = list(set_1)
+    list_2 = list(set_2)
+    new_list = []
 
-	for i in range(0, len(list_1)):
-		for j in range(0, len(list_2)):
-			if (list_1[i] == list_2[j]):
-				new_list.append(list_1[i])
+    for i in range(0, len(list_1)):
+        for j in range(0, len(list_2)):
+            if (list_1[i] == list_2[j]):
+                new_list.append(list_1[i])
 
-	return dict.fromkeys(new_list)
+    return dict.fromkeys(new_list)
