c6910827103f44941ccc0ab36b16268dc3ab073e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-06 23:27:10 -0300

    102 done?

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    
+    new_dic = a_dictionary.copy()
+    for key, target_value in new_dic.items():
+        if target_value == value:
+            del a_dictionary[key]
+    new_dic.copy()
+    return new_dic
