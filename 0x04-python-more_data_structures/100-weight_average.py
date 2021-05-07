4ce2985fbbc308643986f95b3a7100022927cef2
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-06 22:57:18 -0300

    ?

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    res = 0
+    temp = 0
+    divi = 0
+    for item in my_list:
+        for j in range(0, len(item)):
+            if (j == 0):
+                temp = item[j]
+                continue
+            temp *= item[j]
+            divi += item[j]
+        res +=temp
+
+    return res / temp
