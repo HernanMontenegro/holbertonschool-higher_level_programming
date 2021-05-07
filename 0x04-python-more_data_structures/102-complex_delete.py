fd8a3fab5800e1ff88ed0a87f381371b7cb5c5b9
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-06 23:39:56 -0300

    asd

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k,v in a_dictionary.copy().items():
+    for k, v in a_dictionary.copy().items():
         if v == value:
             a_dictionary.pop(k)
     return a_dictionary
