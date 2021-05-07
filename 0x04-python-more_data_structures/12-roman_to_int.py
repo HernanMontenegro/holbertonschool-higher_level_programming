a3147dc9d8a886dec6b72ee4dd31e98e496eb39c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-06 23:18:27 -0300

    asd

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,14 +4,26 @@ def roman_to_int(roman_string):
     value = 0
     if type(roman_string) is not str or roman_string is None:
         return (0)
-    vals = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    traduction = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50, 
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
     for i in range(len(roman_string)):
-        value = vals[roman_string[i]]
+        value = traduction[roman_string[i]]
+
         if i < (len(roman_string) - 1):
-            if value < vals[roman_string[i + 1]]:
+            if value < traduction[roman_string[i + 1]]:
                 res -= value
             else:
                 res += value
         else:
             res += value
+
     return (res)
