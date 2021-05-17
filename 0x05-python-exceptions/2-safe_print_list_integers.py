67b1d7ebc855de96e88d2fd788d76cea1389936e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-17 12:20:49 -0300

    2 fix?

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,9 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             chars_printed += 1
             i += 1
-        except IndexError:
-            print("")
-            return chars_printed
         except (ValueError, TypeError):
             i += 1
             continue
