1826aba14b427e588993d360d22e236ab13597ef
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-17 10:28:48 -0300

    0 done!

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    try:
+        while i != x:
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
+        print("")
+        return i
+    except IndexError:
+        print("")
+        return i
+ 
