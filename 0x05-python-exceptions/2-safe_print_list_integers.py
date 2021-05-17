715495dbafcd79ba63e878b107bef9ce0e595294
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-17 11:01:34 -0300

    2 fix

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    chars_printed = 0
+    while i != x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            chars_printed += 1
+            i += 1
+        except IndexError:
+            print("")
+            return chars_printed
+        except (ValueError, TypeError):
+            i += 1
+            continue
+    print("")
+    return chars_printed
