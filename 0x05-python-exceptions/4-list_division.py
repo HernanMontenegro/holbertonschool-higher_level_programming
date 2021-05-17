42fef166b5345b5ab347e3eca1aa0e3ad5a8e16d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-17 15:42:02 -0300

    4?

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,5 +11,6 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         except IndexError:
             print("out of range")
-        divisions.append(0)
+        finally:
+            divisions.append(0)
     return divisions
