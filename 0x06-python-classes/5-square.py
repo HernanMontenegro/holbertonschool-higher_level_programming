003ff5665f99ae9cbc9512c92814e49b72cfad11
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-18 13:58:08 -0300

    5 done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -20,6 +20,8 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        if (self.__size == 0):
+            print("")
         for i in range(0, self.__size):
             for j in range(0, self.__size):
                 print("#", end="")
