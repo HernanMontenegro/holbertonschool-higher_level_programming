319162ed62f540bbac8c091d8d478ca060d7aa10
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-18 17:11:00 -0300

    6 fix?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,6 +21,7 @@ class Square:
     def my_print(self):
         if (self.__size == 0):
             print("")
+            return
         for k in range(0, self.__position[1]):
             print("")
         for i in range(0, self.__size):
