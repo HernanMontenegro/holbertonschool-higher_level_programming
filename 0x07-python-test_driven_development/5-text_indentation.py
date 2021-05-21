948ac994cc3c8df0d064591259600fb01bf4b4fc
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-21 11:12:00 -0300

    5 done?

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""Executable command
+
+    You can test this function in
+    /test/5-text_indentation.txt
+"""
+
+
+def text_indentation(text):
+    i = 0
+    c = 'a'
+    if (type(text) != str):
+        raise TypeError("text must be a string")
+
+    while i < len(text):
+        c = text[i]
+        print(c, end="")
+        if (c == '.' or c == '?' or c == ':'):
+            if (i == len(text) - 1):
+                break
+            print("\n")
+            while (i < len(text)):
+                if (text[i + 1] != ' '):
+                    break
+                i += 1
+        i += 1
