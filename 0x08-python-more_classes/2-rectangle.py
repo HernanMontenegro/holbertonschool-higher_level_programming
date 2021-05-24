bf62934dcdff12cc5c997a357fe3bca335380756
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-24 09:52:37 -0300

    2 done

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Executable command
+"""
+
+
+class Rectangle:
+    """ Class that defines a
+    rectangle
+    """
+
+    __width = 0
+    __height = 0
+
+    def __init__(self, width=0, height=0):
+        """ Called when a new instance is created """
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+    
+    def perimeter(self):
+        return  (self.__height * 2) + (self.__width * 2)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if (type(value) != int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
