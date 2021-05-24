d6b41be88c49ebd01770fa404daa0e0244f9ae47
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-05-24 19:22:15 -0300

    a

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,9 +14,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ Called when a new instance is created """
-
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         return
 
     @property
