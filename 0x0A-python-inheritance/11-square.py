34dd4540032c839c24bd06bd85a559fb27d50327
Hernan Montenegro <77987989+HernanMontenegro@users.noreply.github.com>
2021-06-01 20:44:21 -0300

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,18 +2,7 @@
 ''' Executable command '''
 
 
-class BaseGeometry:
-    ''' A Base Geometry class '''
-    def area(self):
-        ''' the area of the shape '''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        ''' validates an int value '''
-        if (type(value) != int):
-            raise TypeError("{} must be an integer".format(name))
-        elif (value <= 0):
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
