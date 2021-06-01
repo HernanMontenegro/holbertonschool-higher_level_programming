68a43b13fb5d982f7b3c9307975204ff10632384
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-01 20:34:28 -0300

    3 asd

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+''' Executable Command '''
 
 
 def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+    ''' is kind of class '''
+    return isinstance(obj, a_class) or issubclass(a_class, type(obj))
