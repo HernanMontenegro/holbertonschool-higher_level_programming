2dbaebed8746b5bdef4dfd09fbb1d47166aed4e6
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-01 19:40:33 -0300

    wewe

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -15,6 +15,7 @@ class BaseGeometry:
         elif (value <= 0):
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     ''' Defines a rectangle '''
 
