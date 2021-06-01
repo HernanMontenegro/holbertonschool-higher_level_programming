fac4665b8d9c895ba49c9c792017df46bf2a34a4
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-01 19:43:15 -0300

    asdddsds

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -24,8 +24,8 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         ''' The init method '''
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
 
         self.__width = width
         self.__height = height
