27d578136be5977869c739bd4af0cddd9f05a579
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-01 19:19:50 -0300

    asd

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+''' Executable command '''
 
 
 def inherits_from(obj, a_class):
+    ''' Inherits from '''
     if (type(obj) is a_class):
         return (False)
     return issubclass(type(obj), a_class)
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,6 +3,8 @@
 
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     ''' Defines a rectangle '''
 
