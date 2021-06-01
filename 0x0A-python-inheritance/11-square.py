fd957f356a3ad2b451c913ad8d1471653d22ca33
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-01 20:57:58 -0300

    asd

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -2,27 +2,7 @@
 ''' Executable command '''
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
-
-class Rectangle(BaseGeometry):
-    ''' Defines a rectangle '''
-
-    def __init__(self, width, height):
-        ''' The init method '''
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        ''' Calculate the area '''
-        return self.__width * self.__height
-
-    def __str__(self):
-        ''' String returned on printing '''
-        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+Rectangle = __import__('7-base_geometry').Rectangle
 
 
 class Square(Rectangle):
