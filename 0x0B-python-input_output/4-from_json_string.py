ee0a5495e3d16312690e06ba6ff4a4811a30e4c4
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-02 10:47:11 -0300

    soy una figura

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Executable command '''
+
+
+import json
+
+
+def from_json_string(my_str):
+    ''' returns an object represented by a JSON string '''
+    return json.loads(my_str)
