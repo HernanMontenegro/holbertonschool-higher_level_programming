a91b36885d4c40e1327fe4f6bfe3564b851f1e75
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-02 12:17:56 -0300

    10 done

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' Executable command '''
+
+
+class Student:
+    ''' Student class definition '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=[]):
+        ''' retrieves a dictionary representation '''
+        my_dict = {}
+        if (attrs != []):
+            for item in attrs:
+                if (item == "first_name"):
+                    my_dict['first_name'] = self.first_name
+                elif (item == "last_name"):
+                    my_dict['last_name'] = self.last_name
+                elif (item == "age"):
+                    my_dict['age'] = self.age
+        else:
+            my_dict = self.__dict__
+        return my_dict
