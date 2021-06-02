00b7f83af378cde2974522c3158b9c6780564c60
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-02 10:32:48 -0300

    it's really allowed to import a module?

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+''' Executable command '''
+
+
+import json
+
+
+def to_json_string(my_obj):
+    ''' returns the JSON representation of an object '''
+    return json.dumps(my_obj)
