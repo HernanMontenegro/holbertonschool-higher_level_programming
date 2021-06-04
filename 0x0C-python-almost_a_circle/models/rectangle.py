1f4ac8a03002bc996460ae8b31b7462c1d0ccf62
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-06-04 11:11:47 -0300

    AAAAAAAAAAAAAAAAAA

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,7 +11,6 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         ''' Class constructor '''
         super().__init__(id)
-
         self.width = width
         self.height = height
         self.x = x
@@ -37,23 +36,38 @@ class Rectangle(Base):
         ''' y getter '''
         return self.__y
 
-
     @width.setter
     def width(self, value):
         ''' width setter '''
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        elif (value <= 0):
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
         ''' height setter '''
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        elif (value <= 0):
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @x.setter
     def x(self, value):
         ''' x setter '''
+        if (type(value) is not int):
+            raise TypeError("x must be an integer")
+        elif (value < 0):
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
     def y(self, value):
         ''' y setter '''
+        if (type(value) is not int):
+            raise TypeError("y must be an integer")
+        elif (value < 0):
+            raise ValueError("y must be >= 0")
         self.__y = value
