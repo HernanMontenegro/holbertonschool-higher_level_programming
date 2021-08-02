b51d7e5fffae9f0fb9777a96478e0f37462eb85a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-02 16:47:00 +0000

    2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let argv = process.argv;
+
+if (argv.length == 2)
+    console.log('No argument');
+else if (argv.length == 3)
+    console.log('Argument found');
+else
+    console.log('Arguments found');
