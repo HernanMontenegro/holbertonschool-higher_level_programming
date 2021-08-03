eaed86cbc403bc83a9f58c73fb7acfbf548a2beb
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-03 17:01:07 +0000

    102 uwu

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 
+const fs = require('fs');
+const argv = process.argv;
 
+let str1 = fs.readFileSync(argv[2]).toString();;
+let str2 = fs.readFileSync(argv[3]).toString();;
+
+fs.writeFile(argv[4], str1 + str2, { overwrite: true }, function (err) {
+    if (err) throw err;
+});
