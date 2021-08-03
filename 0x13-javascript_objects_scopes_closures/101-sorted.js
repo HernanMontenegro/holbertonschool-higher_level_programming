a87c69747408605e0deca7ea45aaa8daf5432b0b
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-03 15:45:08 +0000

    101 done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const dict = require('./101-data').dict;
+
+const new_obj = new Object();
+
+for (const key in dict) {
+  if (!new_obj.hasOwnProperty(dict[key])) {
+    new_obj[dict[key]] = [];
+  }
+
+  new_obj[dict[key]].push(key);
+}
+
+console.log(new_obj);
