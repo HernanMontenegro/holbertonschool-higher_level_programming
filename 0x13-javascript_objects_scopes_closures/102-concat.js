629a824b7d24260a7363df3d2f7e4f2da406dd07
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-03 17:02:45 +0000

    requierement fail solved

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 const argv = process.argv;
 
-let str1 = fs.readFileSync(argv[2]).toString();;
-let str2 = fs.readFileSync(argv[3]).toString();;
+const str1 = fs.readFileSync(argv[2]).toString();
+const str2 = fs.readFileSync(argv[3]).toString();
 
 fs.writeFile(argv[4], str1 + str2, { overwrite: true }, function (err) {
-    if (err) throw err;
+  if (err) throw err;
 });
