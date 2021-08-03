d19d4ffed54b4683399615dd27e2dc44db87936a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-03 15:04:00 +0000

    from 4 to 7

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+      if (isNaN(parseInt(w)) || w === 0 || parseInt(w) < 0) { return; }
+      if (isNaN(parseInt(h)) || h === 0 || parseInt(h) < 0) { return; }
+  
+      this.width = w;
+      this.height = h;
+    }
+  
+    print () {
+      console.log(('X'.repeat(this.width) + '\n').repeat(this.height).slice(0, -1));
+    }
+
+    rotate() {
+        let aux_h = this.height;
+
+        this.height = this.width;
+        this.width = aux_h;
+    }
+
+    double() {
+        this.width *= 2;
+        this.height *= 2;
+    }
+};
---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+
+    console.log((c.repeat(this.width) + '\n').repeat(this.height).slice(0, -1));
+  }
+};
