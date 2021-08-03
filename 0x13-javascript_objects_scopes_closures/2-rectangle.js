74b0fb0942e7ad7dfe85205354f20772f2e4de61
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-03 14:19:05 +0000

    uwu

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!usr/bin/node
+
+module.exports = class Rectangle {
+    constructor (w, h) {
+        if (isNaN(parseInt(w)) || w == 0 || parseInt(w) < 0)
+            return;
+        if (isNaN(parseInt(h)) || h == 0 || parseInt(h) < 0)
+            return;
+
+        this.width = w;
+        this.height = h;
+    }
+};
