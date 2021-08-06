3daa879719c18805eb841f8789e4aa3268180607
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-06 01:33:20 +0000

    u.u

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,18 +3,22 @@
 import MySQLdb
 from sys import argv
 
-db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+def main():
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
+    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;"
 
-cursor.execute(sql)
+    cursor.execute(sql)
 
-data = cursor.fetchone()
-
-while (data):
-    print(data)
     data = cursor.fetchone()
 
-db.close()
+    while (data):
+        print(data)
+        data = cursor.fetchone()
+
+    db.close()
+
+if __name__ == "__main__":
+   main()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import MySQLdb
+from sys import argv
+
+def main():
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM states WHERE name = ('{}') ORDER BY states.id ASC;".format(argv[4])
+
+    cursor.execute(sql)
+
+    data = cursor.fetchone()
+
+    while (data):
+        print(data)
+        data = cursor.fetchone()
+
+    db.close()
+
+if __name__ == "__main__":
+   main()
