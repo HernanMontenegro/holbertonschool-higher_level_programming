7ff67c814c6c66474b99cd58f241d22548ffe9e0
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-08 17:11:43 +0000

    a

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,8 +5,7 @@ import MySQLdb
 from sys import argv
 
 
-def main():
-    ''' Main func '''
+if __name__ == "__main__":
     db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
     cursor = db.cursor()
@@ -23,7 +22,3 @@ def main():
         data = cursor.fetchone()
 
     db.close()
-
-
-if __name__ == "__main__":
-    main()
