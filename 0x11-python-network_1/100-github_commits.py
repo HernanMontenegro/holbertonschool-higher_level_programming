d1c7e318eab53c698445fbb6e2a271987a8f7369
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 19:18:49 +0000

    100!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,14 +10,8 @@ if __name__ == '__main__':
     name = sys.argv[2]
     url = 'https://api.github.com/repos/' + name + '/' + repo + '/commits'
     headers = {'Accept': 'application/vnd.github.v3+json'}
-
     req = requests.get(url, headers=headers)
-
     comm = req.json()
-    last_ones = []
 
-    last_indx = len(comm) - 1
-
-    for i in range(last_indx, last_indx - 11, -1):
-        it = comm[i]
+    for it in comm[:10]:
         print(it.get('sha') + ': ' + it.get('author').get('login'))
