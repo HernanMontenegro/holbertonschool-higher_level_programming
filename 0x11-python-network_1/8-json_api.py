f531255689cbe017dca3ff2a91f432da1711a484
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 17:35:38 +0000

    8 done!

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -14,22 +14,12 @@ if __name__ == "__main__":
 
     data = {'q': q}
     req = requests.post(hbtn_url, data=data)
+    try:
+        dic = req.json()
 
-    resp_str = req.text[:-1]
-    vals = resp_str.split(',')
-    return_dict = {}
-
-    if (len(vals) == 1):
-        print("No result")
-        exit()
-
-    for i in range(0, len(vals)):
-        k_v = vals[i].split(':')
-        if (len(k_v) != 2):
-            print("Not a valid JSON")
-            exit()
-        for j in range(0, len(k_v)):
-            k_v[j] = k_v[j].replace('"', '').replace('{', '').replace('}', '')
-        return_dict[k_v[0]] = k_v[1]
-
-    print("[{}] {}".format(return_dict['id'], return_dict['name']))
+        if (len(dic) == 0):
+            print("No result")
+        else:
+            print("[{}] {}".format(dic['id'], dic['name']))
+    except Exception as e:
+        print("Not a valid JSON")
