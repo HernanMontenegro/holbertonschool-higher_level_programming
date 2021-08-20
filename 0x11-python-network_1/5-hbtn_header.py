1a006ff29f78f56d8fa88213b6261a6aa6826435
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 13:45:32 +0000

    uff

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+''' Module 0 '''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+    print(req.headers.__dict__.get('_store').get('x-request-id')[1])
