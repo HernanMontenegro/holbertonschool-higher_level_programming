447d9f99bb373fc5f3b10c7e6e4a4fa354e3b3a0
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 17:50:11 +0000

    minor fixes

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,10 +4,11 @@
 import requests
 import sys
 
+
 if __name__ == '__main__':
     usr_name = sys.argv[1]
     usr_pass = sys.argv[2]
     url = 'https://api.github.com/user'
     headers = {'Accept': 'application/vnd.github.v3+json'}
-    req = requests.post(url, auth=(usr_name, usr_pass), headers=headers)
+    req = requests.get(url, auth=(usr_name, usr_pass), headers=headers)
     print(req.json().get('id'))
