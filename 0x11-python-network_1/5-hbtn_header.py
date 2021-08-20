c43a883d29e484e01bae5615631de6cdab79d707
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 13:48:18 +0000

    good!!

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' Module 0 '''
+''' Module 5 '''
 
 import requests
 import sys
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+''' Module 6 '''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    email = {'email': sys.argv[2]}
+    req = requests.post(sys.argv[1], data=email)
+    print(req.text)
