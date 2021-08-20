f47cb8a0ba8d9b5fbfefb0e7118352540660f24c
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 13:55:36 +0000

    7 done

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+''' Module 7 error code '''
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    req = requests.get(sys.argv[1])
+
+    if (req.status_code >= 400):
+        print("Error code: {:d}".format(req.status_code))
+    else:
+        print(req.text)
