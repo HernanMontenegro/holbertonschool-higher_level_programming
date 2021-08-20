81dab387324805e1530ba5e6e5a329e316ad2955
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 17:25:15 +0000

    ???

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     usr_pass = sys.argv[2]
     url = 'https://api.github.com/user'
 
-    req = requests.get(url, auth=(usr_name, usr_pass))
+    req = requests.post(url, auth=(usr_name, usr_pass))
     print(req.json().get('id'))
