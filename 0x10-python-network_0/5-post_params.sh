f260f3129c6d2cfdb9734e88182e20c394d1c6d7
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 20:29:42 +0000

    fix?

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-curl -s -d "email":"hr@holbertonschool.com" -d "subject":"I will always be here for PLD" -X POST "$1"
+curl -s -d "email":" hr@holbertonschool.com" -d "subject":" I will always be here for PLD" -X POST "$1"
