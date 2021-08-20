72e1324ef70a7a517f19a747ccd09d67a86dd36a
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-08-20 19:23:48 +0000

    ?

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import sys
 if __name__ == '__main__':
     repo = sys.argv[1]
     name = sys.argv[2]
-    url = 'https://api.github.com/repos/{}/{}/commits'.format(repo, name)
+    url = 'https://api.github.com/repos/'+ repo +'/'+ name +'/commits'
     req = requests.get(url)
     comm = req.json()
 
