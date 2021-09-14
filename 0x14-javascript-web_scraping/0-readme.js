d76d3f4d2549707f04ce09833533fa360a270c93
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-14 13:42:57 +0000

    js is fun

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const fs = require('fs')
+
+fs.readFile(argv[2], 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
