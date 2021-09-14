00401f2f71480848c11e3243e097849029dda98d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-14 19:59:33 +0000

    asd

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -15,14 +15,14 @@ req(options, function (err, res, body) {
   }
 });
 
-function Ses (ret_json) {
+function Ses (retJson) {
   let films = 0;
-  const results = ret_json.results;
+  const results = retJson.results;
 
-  for (key in results) {
+  for (const key in results) {
     for (let i = 0; i < results[key].characters.length; i++) {
       const item = results[key].characters[i];
-      if (item == 'https://swapi-api.hbtn.io/api/people/18/') {
+      if (item === 'https://swapi-api.hbtn.io/api/people/18/') {
         films++;
       }
     }
