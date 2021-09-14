7c95fa0631ea52e39e038fbc5649cef68d37582f
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-14 18:49:40 -0300

    5 done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
 const req = require('request');
+const fs = require('fs');
 const av = process.argv;
 const options = {
   url: av[2],
@@ -11,22 +12,8 @@ req(options, function (err, res, body) {
   if (err) {
     console.log(err);
   } else {
-    Ses(JSON.parse(body));
+    fs.writeFile(av[3], body, function (err) {
+      if (err) return console.log(err);
+    });
   }
 });
-
-function Ses (retJson) {
-  let films = 0;
-  const results = retJson.results;
-
-  for (const key in results) {
-    for (let i = 0; i < results[key].characters.length; i++) {
-      const item = results[key].characters[i];
-      if (item === 'https://swapi-api.hbtn.io/api/people/18/') {
-        films++;
-      }
-    }
-  }
-
-  console.log(films);
-}
