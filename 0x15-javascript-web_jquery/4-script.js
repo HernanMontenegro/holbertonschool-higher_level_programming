e0c6585fbca7f42581d00976373f16122c38884d
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-22 09:59:22 -0300

    5 done!

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,11 @@
+$("DIV#toggle_header").click(function() {
+    let header = $("header");
+    if (header.hasClass("red")) {
+        header.removeClass("red");
+        header.addClass("green");
+    }
+    else {
+        header.removeClass("green");
+        header.addClass("red");
+    }
+});
