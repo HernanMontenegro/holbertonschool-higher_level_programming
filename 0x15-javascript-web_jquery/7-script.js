9614991e1e256f8ce50bb3718fe3813e9bb20eb8
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-22 11:00:32 -0300

    this still works?

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,3 +1,3 @@
-fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+window.fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
   .then(resp => resp.json())
   .then(data => $('DIV#character').text(data.name));
