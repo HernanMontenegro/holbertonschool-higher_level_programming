dd7086d34efef626da02d13b348f1f8d3665d5e4
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-09-22 10:31:21 -0300

    101 done!

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    let myUl = $("UL.my_list");
+    $("DIV#add_item").click(() => myUl.append("<li>Item</li>"));
+    $("DIV#remove_item").click(() => $("UL.my_list li:last").remove());
+    $("DIV#clear_list").click(() => myUl.empty());
+ });
